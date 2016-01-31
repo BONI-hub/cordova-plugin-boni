a701c2486b6e2cdb34e0ced078b1a4420fd0237d
Angel Tsvetkov <angel.tsvetkov@hotmail.com>
2016-01-31 21:25:16 +0200

    add commented test, local and live apiKeys

--- www/config.js
+++ www/config.js
@@ -5,7 +5,9 @@ module.exports = {
 		'a32773da-fd5d-11e4-a322-1697f925ec7b'
 	],
 	identifier: 'BoniBeacon',
-	apikey: 'bmtnzdda16k2wled',
+	apikey: 'noxn9y12iygp6xtr', //local
+	//apikey: 'bmtnzdda16k2wled', //test
+	//apikey: 'jvnpnqqa76sfbdrf', //live
 	proximity: {
 		immediate: {
 			name: 'ProximityImmediate',
