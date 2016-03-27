01c9679599303ac98ea51bad7d73a6f0cf7d45f4
theqaangel <angel.tsvetkov@hotmail.com>
2016-03-27 22:38:11 +0300

    Module refactoring

--- spec/beacon-registry.spec.js
+++ spec/beacon-registry.spec.js
@@ -21,4 +21,42 @@ describe("A beacon-registry", function() {
     beaconRegistry.add('test', {});
     expect(beaconRegistry.beacons.length).toEqual(0);
   });
+
+  it("return null if request a beacon without uuid", function() {
+    var beaconRegistry = new BeaconRegistry();
+    beaconRegistry.beacons = [];
+    var beacon = beaconRegistry.get(null, '1', '2');
+    expect(beacon).toEqual(null);
+  });
+
+  it("return null if request a beacon without major id", function() {
+    var beaconRegistry = new BeaconRegistry();
+    beaconRegistry.beacons = [];
+    var beacon = beaconRegistry.get('1111-222-333', null, '2');
+    expect(beacon).toEqual(null);
+  });
+
+  it("return null if request a beacon without minor id", function() {
+    var beaconRegistry = new BeaconRegistry();
+    beaconRegistry.beacons = [];
+    var beacon = beaconRegistry.get('1111-222-333', '1', null);
+    expect(beacon).toEqual(null);
+  });
+
+  it("return null if request a beacon but the registry is empty", function() {
+    var beaconRegistry = new BeaconRegistry();
+    beaconRegistry.beacons = [];
+    var beacon = beaconRegistry.get('1111-222-333', '1', '2');
+    expect(beacon).toEqual(null);
+  });
+
+  it("return beacon if request a beacon that is in the registry", function() {
+    var beaconRegistry = new BeaconRegistry();
+    beaconRegistry.beacons = [];
+    beaconRegistry.add(null, td.registeredBeacon);
+    var beacon = beaconRegistry.get(td.registeredBeacon.uuid, td.registeredBeacon.major, td.registeredBeacon.minor);
+    expect(beacon.uuid).toEqual(td.registeredBeacon.uuid);
+    expect(beacon.major).toEqual(td.registeredBeacon.major);
+    expect(beacon.minor).toEqual(td.registeredBeacon.minor);
+  });
 });
--- www/beacon-registry.js
+++ www/beacon-registry.js
@@ -4,197 +4,214 @@ var Beacon = require('cordova.plugin.boni.beacon'),
   config = require('cordova.plugin.boni.config'),
   _ = require('cordova.plugin.boni.lodash');
 
-/**
- * Event callbacks
- */
-var onFarFromSpot = null;
-var onNearToSpot = null;
-var onImmediateToSpot = null;
 
 /**
  * Collection of all iBeacons in range
  */
-function BeaconRegistry() {}
+var BeaconRegistry = function() {};
 
-BeaconRegistry.prototype.beacons = []; //Array with the iBeacons in range
-
-/**
- * Check whether the iBeacon defined with provided meta data is already registered
- * in the beacon registry.
- * @param  {Beacon} beacon object that represents iBeacon metadata
- * @return {boolean}       true if provided iBeacon is already registered in beacon registry,
- *                         otherwise return false
- */
-BeaconRegistry.prototype.getBeaconFromRegistry = function(uuid, major, minor) {
-
-  if (!uuid || !major || !minor) {
-    /**
-     * If beacon's major id, minor id or uuid is not defined,
-     * return beacon is not exists
-     */
-    return;
-  }
-
-  uuid = uuid.toUpperCase();
-  major = parseInt(major);
-  minor = parseInt(minor);
+BeaconRegistry.prototype = function(){
+  var beacons = [],
+  /**
+   * Event callbacks
+   */
+  _onFarFromSpot = null,
+  _onNearToSpot = null,
+  _onImmediateToSpot = null,
 
   /**
-   * If all input arguments are valid, check whether the beacon already exists
-   * in the beacon registry and return it. Otherwise undefined.
+   * Check whether the iBeacon defined with provided meta data is already registered
+   * in the beacon registry.
+   * @param  {Beacon} beacon object that represents iBeacon metadata
+   * @return {boolean}       true if provided iBeacon is already registered in beacon registry,
+   *                         otherwise return false
    */
-  return _.find(this.beacons, {
-    'uuid': uuid,
-    'major': major,
-    'minor': minor
-  });
-};
+  get = function(uuid, major, minor) {
 
-BeaconRegistry.prototype.applyProximityStrategy = function(beacon) {
-  if (beacon) {
-    var proximityFactor = calculateProximityFactor(beacon);
-
-    if (proximityFactor <= config.proximity.immediate.factor) {
-      beacon.proximity = config.proximity.immediate.name;
-    } else if (proximityFactor >
-      config.proximity.immediate.factor + config.proximity.buffer &&
-      proximityFactor <= config.proximity.near.factor) {
-      beacon.proximity = config.proximity.near.name;
-    } else if (proximityFactor >
-      config.proximity.near.factor + config.proximity.buffer) {
-      beacon.proximity = config.proximity.far.name;
-    } else {
+    if (!uuid || !major || !minor) {
       /**
-       * If the proximityFactor is in any of the buffers, keep the latest proximity
+       * If beacon's major id, minor id or uuid is not defined,
+       * return beacon is not exists
        */
-      beacon.proximity = beacon.previouseProximity;
+      return;
     }
-  }
 
-  function calculateProximityFactor(beacon) {
-    return parseInt(parseInt(100 * beacon.rssi - beacon.tx) / beacon.tx);
-  }
-};
+    uuid = uuid.toUpperCase();
+    major = parseInt(major);
+    minor = parseInt(minor);
 
-BeaconRegistry.prototype.observe = function(beacon) {
+    /**
+     * If all input arguments are valid, check whether the beacon already exists
+     * in the beacon registry and return it. Otherwise undefined.
+     */
+    return _.find(beacons, {
+      'uuid': uuid,
+      'major': major,
+      'minor': minor
+    });
+  },
 
-  function callRegisteredCallback(callback, beacon) {
-    if (_.isFunction(callback) && beacon) {
-      if (beacon.data) {
-        callback(null, beacon.data);
+  applyProximityStrategy = function(beacon) {
+    if (beacon) {
+      var proximityFactor = calculateProximityFactor(beacon);
+
+      if (proximityFactor <= config.proximity.immediate.factor) {
+        beacon.proximity = config.proximity.immediate.name;
+      } else if (proximityFactor >
+        config.proximity.immediate.factor + config.proximity.buffer &&
+        proximityFactor <= config.proximity.near.factor) {
+        beacon.proximity = config.proximity.near.name;
+      } else if (proximityFactor >
+        config.proximity.near.factor + config.proximity.buffer) {
+        beacon.proximity = config.proximity.far.name;
       } else {
-        callback('No data');
+        /**
+         * If the proximityFactor is in any of the buffers, keep the latest proximity
+         */
+        beacon.proximity = beacon.previouseProximity;
       }
     }
-  }
 
-  var currentBeacon = this.getBeaconFromRegistry(beacon.uuid, beacon.major,
-    beacon.minor);
-  /**
-   * Check whether the beacons is registered
-   */
-  if (currentBeacon) {
+    function calculateProximityFactor(beacon) {
+      return parseInt(parseInt(100 * beacon.rssi - beacon.tx) / beacon.tx);
+    }
+  },
 
-    /**
-     * Update iBeacon metadata
-     */
-    currentBeacon.proximity = beacon.proximity;
-    currentBeacon.rssi = beacon.rssi;
-    currentBeacon.tx = beacon.tx;
-    currentBeacon.accuracy = beacon.accuracy;
+  observe = function(beacon) {
 
-    this.applyProximityStrategy(currentBeacon);
-
-    if (currentBeacon.proximity) {
-
-      /**
-       * If previouse proximity is not set or previouse proximity is different
-       * than the current. The idea is to call callback only on change.
-       */
-      if (!currentBeacon.previouseProximity || currentBeacon.proximity !==
-        currentBeacon.previouseProximity) {
-
-        /**
-         * update the previouse proximity
-         */
-        currentBeacon.previouseProximity = currentBeacon.proximity;
-
-        /**
-         * Call the appropreate callbacks if there are registered
-         */
-        switch (currentBeacon.proximity) {
-          case config.proximity.immediate.name:
-            callRegisteredCallback(onImmediateToSpot, currentBeacon);
-            break;
-          case config.proximity.near.name:
-            callRegisteredCallback(onNearToSpot, currentBeacon);
-            break;
-          case config.proximity.far.name:
-            callRegisteredCallback(onFarFromSpot, currentBeacon);
-            break;
-          default:
-
+    function callRegisteredCallback(callback, beacon) {
+      if (_.isFunction(callback) && beacon) {
+        if (beacon.data) {
+          callback(null, beacon.data);
+        } else {
+          callback('No data');
         }
       }
     }
 
-  } else {
+    var currentBeacon = this.get(beacon.uuid, beacon.major,
+      beacon.minor);
     /**
-     * If it is not registered, retrieve its data.
-     * In this way only one server call is initiated to retrieve iBeacon Content (cloud) data.
+     * Check whether the beacons is registered
      */
-    currentBeacon = new Beacon(
-      beacon.uuid,
-      beacon.major,
-      beacon.minor,
-      beacon.proximity,
-      beacon.rssi,
-      beacon.tx,
-      beacon.accuracy
-    );
-    this.applyProximityStrategy(currentBeacon);
-    currentBeacon.getData(this.add);
-  }
-};
+    if (currentBeacon) {
 
-/**
- * Add the beacon into beacon register
- * @param  {[type]} error  error message if there is any, null if there is no errors
- * @param  {Beacon} beacon beacon object that represents beacon meatdata
- */
-BeaconRegistry.prototype.add = function(error, beacon) {
+      /**
+       * Update iBeacon metadata
+       */
+      currentBeacon.proximity = beacon.proximity;
+      currentBeacon.rssi = beacon.rssi;
+      currentBeacon.tx = beacon.tx;
+      currentBeacon.accuracy = beacon.accuracy;
 
-  if (!error) {
+      this.applyProximityStrategy(currentBeacon);
 
-    /**
-     * If there are no errors, add beacon to beacon registry
-     */
-    this.beacons.push(beacon);
-  }
-};
+      if (currentBeacon.proximity) {
 
-/**
- * Register onImmediateToSpot callback
- * @param  {Function} callback function to be called when the user is immediate to spot
- */
-BeaconRegistry.prototype.onImmediateToSpot = function(callback) {
-  onImmediateToSpot = callback;
-};
+        /**
+         * If previouse proximity is not set or previouse proximity is different
+         * than the current. The idea is to call callback only on change.
+         */
+        if (!currentBeacon.previouseProximity || currentBeacon.proximity !==
+          currentBeacon.previouseProximity) {
 
-/**
- * Register onNearToSpot callback
- * @param  {Function} callback function to be called when the user is near to spot
- */
-BeaconRegistry.prototype.onNearToSpot = function(callback) {
-  onNearToSpot = callback;
-};
+          /**
+           * update the previouse proximity
+           */
+          currentBeacon.previouseProximity = currentBeacon.proximity;
 
-/**
- * Register onFarFromSpot callback
- * @param  {Function} callback function to be called when the user is far from spot
- */
-BeaconRegistry.prototype.onFarFromSpot = function(callback) {
-  onFarFromSpot = callback;
-};
+          /**
+           * Call the appropreate callbacks if there are registered
+           */
+          switch (currentBeacon.proximity) {
+            case config.proximity.immediate.name:
+              callRegisteredCallback(_onImmediateToSpot, currentBeacon);
+              break;
+            case config.proximity.near.name:
+              callRegisteredCallback(_onNearToSpot, currentBeacon);
+              break;
+            case config.proximity.far.name:
+              callRegisteredCallback(_onFarFromSpot, currentBeacon);
+              break;
+            default:
+
+          }
+        }
+      }
+
+    } else {
+      /**
+       * If it is not registered, retrieve its data.
+       * In this way only one server call is initiated to retrieve iBeacon Content (cloud) data.
+       */
+      currentBeacon = new Beacon(
+        beacon.uuid,
+        beacon.major,
+        beacon.minor,
+        beacon.proximity,
+        beacon.rssi,
+        beacon.tx,
+        beacon.accuracy
+      );
+      this.applyProximityStrategy(currentBeacon);
+      currentBeacon.getData(this.add);
+    }
+  },
+
+  /**
+   * Add the beacon into beacon register
+   * @param  {[type]} error  error message if there is any, null if there is no errors
+   * @param  {Beacon} beacon beacon object that represents beacon meatdata
+   */
+  add = function(error, beacon) {
+
+    console.log(beacon);
+    console.log(error);
+
+    if (!error) {
+
+      /**
+       * If there are no errors, add beacon to beacon registry
+       */
+      beacons.push(beacon);
+    }
+  },
+
+  /**
+   * Register onImmediateToSpot callback
+   * @param  {Function} callback function to be called when the user is immediate to spot
+   */
+  onImmediateToSpot = function(callback) {
+    _onImmediateToSpot = callback;
+  },
+
+  /**
+   * Register onNearToSpot callback
+   * @param  {Function} callback function to be called when the user is near to spot
+   */
+  onNearToSpot = function(callback) {
+    _onNearToSpot = callback;
+  },
+
+  /**
+   * Register onFarFromSpot callback
+   * @param  {Function} callback function to be called when the user is far from spot
+   */
+  onFarFromSpot = function(callback) {
+    _onFarFromSpot = callback;
+  };
+
+  return {
+    beacons: beacons,
+    add: add,
+    applyProximityStrategy: applyProximityStrategy,
+    observe: observe,
+    get: get,
+    onFarFromSpot: onFarFromSpot,
+    onNearToSpot: onNearToSpot,
+    onImmediateToSpot: onImmediateToSpot
+  };
+}();
+
 
 module.exports = BeaconRegistry;
